5bf976ee99a999b9e3975dd3cb219cf606ee5bce
chianxintong <xintong239@gmail.com>
2024-03-20 01:25:54 +0800

    successfully created account using firebase without email verification

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,11 +4,11 @@
       <h3>Join us in protecting our environment and paving the way for a sustainable future where the well-being of our planet is at the forefront of our actions and decisions.</h3>
     
       <form id="signupform">
-        <input type="text" id="name" required="" placeholder="Name">
-        <input type="text" id="username" required="" placeholder="Username">
-        <input type="email" id="email" required="" placeholder="Email">
-        <input type="password" id="password" required="" placeholder="Password">
-        <input type="password" id="cfmpassword" required="" placeholder="Confirm Password">
+        <input type="text" id="name" required="" placeholder="Name" v-model="name">
+        <input type="text" id="username" required="" placeholder="Username" v-model="username">
+        <input type="email" id="email" required="" placeholder="Email" v-model="email">
+        <input type="password" id="password" required="" placeholder="Password" v-model="password">
+        <input type="password" id="cfmpassword" required="" placeholder="Confirm Password" v-model="cfmpassword">
       </form>
 
       <button id = "signupbutton" type="button" v-on:click="signup">Sign Up</button>
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-import firebaseApp from '../../firebaseConfig.js'
+import firebaseApp from '../firebaseConfig.js'
 import { getFirestore, doc, setDoc } from 'firebase/firestore'
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth' // firebase authentication
 const auth = getAuth(firebaseApp);
@@ -29,21 +29,24 @@ export default {
       username: '',
       email: '',
       password: '',
-      cfmPassword: '',
+      cfmpassword: '',
     }
   },
   methods: {
     async signup() {
       console.log("in signup")
 
-      // check passwords
-      if (this.password !== this.cfmPassword) {
+      /* check passwords */
+      if (this.password !== this.cfmpassword) {
         alert("Passwords do not match.");
       }
 
       try {
+        /* creating account using email and password */
         const userInfo = await createUserWithEmailAndPassword(auth, this.email, this.password);
         const user = userInfo.user;
+
+        /* creating users collection in db to store other user info subsequently*/
         await setDoc(doc(db, "users", user.uid), {
           name: this.name,
           username: this.username,
@@ -61,4 +64,4 @@ export default {
 
 
 <style>
-</style>../firebaseConfig.js
+</style>
---
+++ src/components/SignUp/EmailVerification.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+      <h1>Email Verification</h1>
+    </div>
+</template>
