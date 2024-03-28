01719edcf2d9836b4bb5e4a26a92129b1b7fd2fc
ugchongg <121675791+ugchongg@users.noreply.github.com>
2024-03-28 16:33:15 +0800

    Added search bar and button in homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,16 +1,13 @@
 <template>
-    <div>
-      <h1>Home Page</h1>
-    </div>
-
-    <Logout />
+    <SearchBar />
 </template>
 
 <script>
-import Logout from '../components/Logout.vue'
+import SearchBar from '../components/SearchBar.vue'
+
 export default {
   components: {
-    Logout
+    SearchBar
   }
 }
 </script>
