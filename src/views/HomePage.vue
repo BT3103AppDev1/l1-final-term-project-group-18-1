777d8e9c7ca41176802ce6036ab75cd498bc8561
ugchongg <121675791+ugchongg@users.noreply.github.com>
2024-03-28 16:41:44 +0800

    added log out button back in homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,16 @@
 <template>
     <SearchBar />
+    <LogOut />
 </template>
 
 <script>
 import SearchBar from '../components/SearchBar.vue'
+import LogOut from '../components/LogOut.vue'
 
 export default {
   components: {
-    SearchBar
+    SearchBar,
+    Logout
   }
 }
 </script>
