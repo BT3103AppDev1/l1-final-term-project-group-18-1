e95f5535615b89fa1e73e20cbb1aa2a47da8e181
chianxintong <xintong239@gmail.com>
2024-04-14 00:38:46 +0800

    preliminary sync with google calendar

--- src/views/Calendar/CalendarPage.vue
+++ src/views/Calendar/CalendarPage.vue
@@ -1,22 +1,40 @@
 <template>
   <div>
-    <h1>Calendar Page</h1>
-    <button @click="signInWithGoogle">Sign in with Google</button>
-    <button @click="goToSyncedCalendar">Go to Synced Calendar</button>
+    <GoogleLoginButton @authenticated="listEvents"/>  <!-- Listen to authenticated event -->
+    <ul v-if="events.length">
+      <li v-for="event in events" :key="event.id">{{ event.summary }} - {{ event.start.dateTime }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import GoogleCalendar from '../../GoogleCalendar.js';
+import GoogleLoginButton from '../../components/GoogleLoginButton.vue'
 
 export default {
-  methods: {
-    signInWithGoogle() {
-      window.location = GoogleCalendar.getAuthUrl();
-    },
-    goToSyncedCalendar() {
-      this.$router.push({ name: 'SyncedCalendar' });
-    },
+  name: 'CalendarPage',
+  components: {
+    GoogleLoginButton
   },
+  data() {
+    return {
+      events: []
+    };
+  },
+  methods: {
+    listEvents() {
+      window.gapi.client.calendar.events.list({
+        'calendarId': 'primary',
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime'
+      }).then(response => {
+        this.events = response.result.items;
+      }).catch(error => {
+        console.error("Error fetching events:", error);
+      });
+    }
+  }
 }
 </script>
