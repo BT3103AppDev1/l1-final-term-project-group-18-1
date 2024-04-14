add536f2bdd14ca9c8a168a8f77e613f60059c97
Angela Y. <angelayam0309@gmail.com>
2024-04-14 16:55:05 +0800

    map ver 1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import './assets/main.css'
+import VueGoogleMaps from '@fawmi/vue-google-maps';
+import VueGeoLocation from 'vue-browser-geolocation';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.config.productionTip = false;
+app.use(router);
+// app.use(VueGeoLocation);
+// app.use(VueGoogleMaps, {
+//   load: {
+//     key: 'VUE_APP_GOOGLE_API_KEY',
+//     // other options if needed
+//   },
+// });
+
+app.mount('#app');
---
+++ src/views/Resources/ThriftLocator.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>Map Page</h1>
+      <h3>thrift store west</h3>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d255277.61295008776!2d103.71080971408264!3d1.348881037800735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sthrift%20stores%20west!5e0!3m2!1sen!2ssg!4v1712547586190!5m2!1sen!2ssg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <h3>thrift store north</h3>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d255277.6141205669!2d103.7108088502294!3d1.3488698808860415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sthrift%20stores%20north!5e0!3m2!1sen!2ssg!4v1712547664048!5m2!1sen!2ssg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <h3>thrift store east</h3>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d127640.7333761056!2d103.80391031024321!3d1.311632856499252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sthrift%20store%20east!5e0!3m2!1sen!2ssg!4v1713076220690!5m2!1sen!2ssg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d255277.61529102668!2d103.71080798637608!3d1.3488587240635461!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sthrift%20stores%20east!5e0!3m2!1sen!2ssg!4v1712547762965!5m2!1sen!2ssg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+      <!-- there is always missing pins -->
+      <h3>thrift store south</h3>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d255277.6164614671!2d103.71080712252278!3d1.348847567333274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sthrift%20stores%20south!5e0!3m2!1sen!2ssg!4v1712547814004!5m2!1sen!2ssg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <h3>thrift store central</h3>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d255277.6176318882!2d103.71080625866952!3d1.3488364106952124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sthrift%20stores%20central!5e0!3m2!1sen!2ssg!4v1712547837732!5m2!1sen!2ssg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</template>
+
+  <script>
+  export default {
+    name: 'ThriftLocator',
+  }
+  </script>
