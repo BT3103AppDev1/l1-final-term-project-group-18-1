40632c8cc0cd8e69e974c2852b211ba01d123f75
chianxintong <xintong239@gmail.com>
2024-04-25 23:02:27 +0800

    fixed contact us page UI

--- src/views/Settings/ContactUs.vue
+++ src/views/Settings/ContactUs.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Contact Us</h1>
+    <h1 class="title">Contact Us</h1>
     <h2>Get in touch!</h2>
     <div class="email"> 
       <font-awesome-icon icon="envelope" class="emailicon" />
@@ -31,40 +31,37 @@
 
 <style scoped>
 /* Use the parent component's CSS to target the child component */
-.settings-wrapper h1 {
-  margin-right: 960px;
+.settings-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start; /* Aligns children to the start of the container */
 }
 
-
+.settings-wrapper h1,
 .settings-wrapper h2 {
-  margin-right: 920px;
-  margin-bottom: 30px;
-  font-weight: normal;
+  text-align: left;
+  margin-bottom: 30px; /* Only if you need space between the header and sub-header */
 }
 
 .settings-wrapper .email,
 .settings-wrapper .telegram {
-    display: flex;
-    align-items: center;
-    margin-left: 50px;
-    margin-top: 10px;
-  }
-  
-.settings-wrapper .emailicon {
-    margin-right: 10px;
-    size: 20px;
-    color: var(--primary-color);
-    font-size: 20px;
-  }
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+}
 
-  .settings-wrapper .telegramicon {
-    margin-right: 10px;
-    size: 20px;
-    color: #24A1DE;
-    font-size: 20px;
-  }
+.settings-wrapper .emailicon,
+.settings-wrapper .telegramicon {
+  margin-right: 10px;
+  font-size: 20px; /* 'size' is not a valid CSS property for font-size */
+}
 
-  .settings-wrapper .contact-info {
-    font-weight: normal; 
+.settings-wrapper .contact-info {
+  font-weight: normal;
+}
+
+
+.title {
+  color: #457247;
 }
 </style>
