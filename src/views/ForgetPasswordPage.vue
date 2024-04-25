55f358419ae7366b86198676776a0ffc9d6adbd7
chianxintong <xintong239@gmail.com>
2024-04-25 15:43:54 +0800

    fixed forget password page UI

--- src/views/ForgetPasswordPage.vue
+++ src/views/ForgetPasswordPage.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
+    <br>
     <img src="../assets/pw_reset.png">
-    <h1>Forgot Password</h1>
+    <h1 class="title">Forgot Password</h1>
     <h3>Enter your email and we'll send you a link to rest your password.</h3><br>
     <form @submit.prevent="submitForm">
       <input class="email" type="email" v-model="email" placeholder="Enter your email" required>
       <button type="submit">Submit</button>
     </form>
-    <p v-if="message">{{ message }}</p>
+    <p class="message" v-if="message">{{ message }}</p>
   </div>
 </template>
 
@@ -43,5 +44,9 @@ export default {
   img {
     width: 10%;
     height: auto; /* keeps the image aspect ratio */
+  }
+  .title,
+  .message {
+    color: #457247 !important;
   }
 </style>
